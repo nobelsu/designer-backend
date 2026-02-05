76981f7304a136d22fbd24a5b03aea0868e87c8a
nobelsu <nobel@suhendra.com>
2026-02-05 01:40:35 +0000

    Enabled all origins

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ from typing import Union
 from dotenv import load_dotenv
 from vercel.sandbox import Sandbox
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 load_dotenv('.env.local')
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Success!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/health")
+def read_root():
+    return {"health": "Server is up and running!"}
 
 @app.get("/sandbox")
 def createSandbox():

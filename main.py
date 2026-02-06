ddf1fd9344d74bfb5623e333d4135db6ebb4d316
nobelsu <nobel@suhendra.com>
2026-02-06 21:10:30 +0000

    Added sandbox refreshing using snapshot and directory viewing for observability

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from vercel.sandbox import Sandbox
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from pydantic import BaseModel
+
+class RefreshProps(BaseModel):
+    sandboxId: str
+
+class DirectoryProps(BaseModel):
+    sandboxId: str 
+    path: str
 
 app = FastAPI()
 
@@ -41,4 +49,45 @@ def createSandbox():
     return {
         "sandboxID": sandbox.sandbox_id,
         "preview": sandbox.domain(3000),
+    }
+
+@app.post("/refresh")
+def refreshSandbox(props: RefreshProps):
+    sandbox = Sandbox.get(
+        sandbox_id=props.sandboxId,
+        team_id=os.getenv("VERCEL_TEAM_ID"),
+        project_id=os.getenv("VERCEL_PROJECT_ID"),
+        token=os.getenv("VERCEL_TOKEN"),
+    )
+
+    snapshot = sandbox.snapshot()
+
+    newSandbox = Sandbox.create(
+        ports=[3000], 
+        source={"type": "snapshot", "snapshot_id": snapshot.snapshot_id},
+        team_id=os.getenv("VERCEL_TEAM_ID"),
+        project_id=os.getenv("VERCEL_PROJECT_ID"),
+        token=os.getenv("VERCEL_TOKEN"),
+        timeout=10 * 60 * 1000
+    )
+
+    return {
+        "sandboxID": newSandbox.sandbox_id,
+        "preview": newSandbox.domain(3000),
+    }
+
+@app.post("/directory")
+async def viewDirectory(props: DirectoryProps):
+    sandbox = Sandbox.get(
+        sandbox_id=props.sandboxId,
+        team_id=os.getenv("VERCEL_TEAM_ID"),
+        project_id=os.getenv("VERCEL_PROJECT_ID"),
+        token=os.getenv("VERCEL_TOKEN"),
+    )
+
+    val = sandbox.run_command("ls", [props.path])
+
+    return { 
+        "STDOUT": val.stdout(),
+        "STDERR": val.stderr()
     }
